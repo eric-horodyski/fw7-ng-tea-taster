829cf970eef5296811321728243497a92cbe76d1
Eric Horodyski <horodyski@ionic.io>
2023-03-13 14:14:05 -0400

    step(2): add platforms

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'tea-taster',
+  appId: 'io.horodyski.teataster',
+  appName: 'Tea Taster',
   webDir: 'www',
   bundledWebRuntime: false,
 };
